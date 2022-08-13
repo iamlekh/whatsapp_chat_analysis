ecc7345ba00c84f9e2e818104581e6b208fc84ac
iamlekh <48363134+iamlekh@users.noreply.github.com>
2022-08-13 22:19:27 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -38,125 +38,121 @@ with col4:
 
 uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
-    try:
-        dataframe = pd.read_csv(
-            uploaded_file,
-            sep="delimiter",
-            skip_blank_lines=True,
-            header=None,
-            engine="python",
-        )
+    # try:
+    dataframe = pd.read_csv(
+        uploaded_file,
+        sep="delimiter",
+        skip_blank_lines=True,
+        header=None,
+        engine="python",
+    )
 
-        st.title("clean data")
-        df1 = data.clean_data(dataframe)
-        st.dataframe(df1)
+    st.title("clean data")
+    df1 = clean_data(dataframe)
+    st.dataframe(df1)
 
-        sender_msg = data.sender_msg_count(df1)[0]
+    sender_msg = sender_msg_count(df1)[0]
 
-        total_menber = (
-            f"You have total -- {len(sender_msg.index)} -- members in the group."
-        )
-        st.subheader(total_menber)
+    total_menber = f"You have total -- {len(sender_msg.index)} -- members in the group."
+    st.subheader(total_menber)
 
-        st.pyplot(data.activity_wrt_time(df1))
+    st.pyplot(activity_wrt_time(df1))
 
-        avg_msg = data.top_active_days_avgmsg(df1)[1]
-        st.subheader(f"Average messages per day {avg_msg}")
+    avg_msg = top_active_days_avgmsg(df1)[1]
+    st.subheader(f"Average messages per day {avg_msg}")
 
-        st.bar_chart(data.top_active_days_avgmsg(df1)[0])
+    st.bar_chart(top_active_days_avgmsg(df1)[0])
 
-        iterator_marks = iter(data.sender_msg_count(df1)[0].index)
-        l = len(data.sender_msg_count(df1)[0].index)
-        for _ in range(l):
+    iterator_marks = iter(sender_msg_count(df1)[0].index)
+    l = len(sender_msg_count(df1)[0].index)
+    for _ in range(l):
 
-            if (l % 2) == 0:
-                try:
-                    col1, col2 = st.columns(2)
-                    a = next(iterator_marks)
-                    b = next(iterator_marks)
-                    col1.metric(
-                        a,
-                        data.sender_msg_count(df1)[0]["message counts"][a],
-                        data.sender_msg_count(df1)[0]["message counts"][a]
-                        - data.sender_msg_count(df1)[1],
-                    )
-                    col2.metric(
-                        b,
-                        data.sender_msg_count(df1)[0]["message counts"][b],
-                        data.sender_msg_count(df1)[0]["message counts"][b]
-                        - data.sender_msg_count(df1)[1],
-                    )
-                except StopIteration:
-                    break
+        if (l % 2) == 0:
+            try:
+                col1, col2 = st.columns(2)
+                a = next(iterator_marks)
+                b = next(iterator_marks)
+                col1.metric(
+                    a,
+                    sender_msg_count(df1)[0]["message counts"][a],
+                    sender_msg_count(df1)[0]["message counts"][a]
+                    - sender_msg_count(df1)[1],
+                )
+                col2.metric(
+                    b,
+                    sender_msg_count(df1)[0]["message counts"][b],
+                    sender_msg_count(df1)[0]["message counts"][b]
+                    - sender_msg_count(df1)[1],
+                )
+            except StopIteration:
+                break
+        else:
+            try:
+                col1, col2 = st.columns(2)
+                a, b = next(iterator_marks), next(iterator_marks)
+                col1.metric(
+                    a,
+                    sender_msg_count(df1)[0]["message counts"][a],
+                    sender_msg_count(df1)[0]["message counts"][a]
+                    - sender_msg_count(df1)[1],
+                )
+                col2.metric(
+                    b,
+                    sender_msg_count(df1)[0]["message counts"][b],
+                    sender_msg_count(df1)[0]["message counts"][b]
+                    - sender_msg_count(df1)[1],
+                )
+            except StopIteration:
+                break
             else:
                 try:
-                    col1, col2 = st.columns(2)
-                    a, b = next(iterator_marks), next(iterator_marks)
-                    col1.metric(
-                        a,
-                        data.sender_msg_count(df1)[0]["message counts"][a],
-                        data.sender_msg_count(df1)[0]["message counts"][a]
-                        - data.sender_msg_count(df1)[1],
-                    )
-                    col2.metric(
-                        b,
-                        data.sender_msg_count(df1)[0]["message counts"][a],
-                        data.sender_msg_count(df1)[0]["message counts"][b]
-                        - data.sender_msg_count(df1)[1],
-                    )
-                except StopIteration:
-                    break
-                else:
                     c = next(iterator_marks)
                     st.metric(
                         c,
-                        data.sender_msg_count(df1)[0]["message counts"][c],
-                        data.sender_msg_count(df1)[0]["message counts"][c]
-                        - data.sender_msg_count(df1)[1],
+                        sender_msg_count(df1)[0]["message counts"][c],
+                        sender_msg_count(df1)[0]["message counts"][c]
+                        - sender_msg_count(df1)[1],
                     )
+                except StopIteration:
+                    break
+    st.subheader("user vs message")
 
-        st.subheader("user vs message")
+    st.bar_chart(sender_msg)
 
-        st.bar_chart(sender_msg)
+    st.subheader(f"Total media file shared -- {total_media_shared(df1)[0]}")
 
-        st.subheader(f"Total media file shared -- {data.total_media_shared(df1)[0]}")
+    st.table(total_media_shared(df1)[1])
 
-        st.table(data.total_media_shared(df1)[1])
+    st.pyplot(word_cloud(df1)[0])
 
-        st.pyplot(data.word_cloud(df1)[0])
+    st.table(emojie_count(df1))
 
-        st.table(data.emojie_count(df1))
+    st.markdown("""---""")
 
-        st.markdown("""---""")
+    col1, col2, col3, col4 = st.columns(4)
 
-        col1, col2, col3, col4 = st.columns(4)
+    with col1:
+        st.write(f"Total words in the chat --  {word_letter_counter(df1)[3]}")
+    with col2:
+        st.write(f"Total letters in the chat --  {word_letter_counter(df1)[2]}")
 
-        with col1:
-            st.write(f"Total words in the chat --  {data.word_letter_counter(df1)[3]}")
-        with col2:
-            st.write(
-                f"Total letters in the chat --  {data.word_letter_counter(df1)[2]}"
-            )
+    with col3:
+        st.write(f"Average words per message -- {word_letter_counter(df1)[1]}")
+    with col4:
+        st.write(f"average letters per message   -- {word_letter_counter(df1)[0]}")
 
-        with col3:
-            st.write(f"Average words per message -- {data.word_letter_counter(df1)[1]}")
-        with col4:
-            st.write(
-                f"average letters per message   -- {data.word_letter_counter(df1)[0]}"
-            )
+    st.markdown("""---""")
 
-        st.markdown("""---""")
+    st.pyplot(time_series(df1)[0])
+    st.markdown("""---""")
+    st.subheader(time_series(df1)[1])
+    st.subheader(time_series(df1)[2])
+    st.subheader(time_series(df1)[3])
 
-        st.pyplot(data.time_series(df1)[0])
-        st.markdown("""---""")
-        st.subheader(data.time_series(df1)[1])
-        st.subheader(data.time_series(df1)[2])
-        st.subheader(data.time_series(df1)[3])
+    st.markdown("""---""")
+    st.subheader(f"total deleted messages {word_letter_counter(df1)[4]}")
+    st.markdown("""---""")
 
-        st.markdown("""---""")
-        st.subheader(f"total deleted messages {data.word_letter_counter(df1)[4]}")
-        st.markdown("""---""")
-
-        st.pyplot(data.word_cloud(df1)[1])
-    except Exception as e:
-        st.subheader("please upload the doc in proper format")
+    st.pyplot(word_cloud(df1)[1])
+    # except Exception as e:
+    # st.subheader("please upload the doc in proper format")
