cf766f86601e8855baad6ffda93d32982f3dd596
iamlekh <48363134+iamlekh@users.noreply.github.com>
2022-08-13 19:17:30 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,162 @@
+import streamlit as st
+import pandas as pd
+import data
+import numpy as np
+import matplotlib.pyplot as plt
+
+st.title("WHATSAPP CHAT ANALYSIS")
+
+col1, col2, col3, col4 = st.columns(4)
+
+with col1:
+    st.header("Step 1")
+    st.caption("Click on Options")
+    st.image(
+        "https://raw.githubusercontent.com/iamlekh/whatsapp-chat-analyser/main/app/static/img/step1.png",
+    )
+
+with col2:
+    st.header("Step 1")
+    st.caption("Click on More")
+    st.image(
+        "https://raw.githubusercontent.com/iamlekh/whatsapp-chat-analyser/main/app/static/img/step2.png"
+    )
+
+with col3:
+    st.header("Step 1")
+    st.caption("Click on Export Chat")
+    st.image(
+        "https://raw.githubusercontent.com/iamlekh/whatsapp-chat-analyser/main/app/static/img/step3.png"
+    )
+
+with col4:
+    st.header("Step 1")
+    st.caption("Click on Without Media")
+    st.image(
+        "https://raw.githubusercontent.com/iamlekh/whatsapp-chat-analyser/main/app/static/img/step4.png"
+    )
+
+uploaded_file = st.file_uploader("Choose a file")
+if uploaded_file is not None:
+    try:
+        dataframe = pd.read_csv(
+            uploaded_file,
+            sep="delimiter",
+            skip_blank_lines=True,
+            header=None,
+            engine="python",
+        )
+
+        st.title("clean data")
+        df1 = data.clean_data(dataframe)
+        st.dataframe(df1)
+
+        sender_msg = data.sender_msg_count(df1)[0]
+
+        total_menber = (
+            f"You have total -- {len(sender_msg.index)} -- members in the group."
+        )
+        st.subheader(total_menber)
+
+        st.pyplot(data.activity_wrt_time(df1))
+
+        avg_msg = data.top_active_days_avgmsg(df1)[1]
+        st.subheader(f"Average messages per day {avg_msg}")
+
+        st.bar_chart(data.top_active_days_avgmsg(df1)[0])
+
+        iterator_marks = iter(data.sender_msg_count(df1)[0].index)
+        l = len(data.sender_msg_count(df1)[0].index)
+        for _ in range(l):
+
+            if (l % 2) == 0:
+                try:
+                    col1, col2 = st.columns(2)
+                    a = next(iterator_marks)
+                    b = next(iterator_marks)
+                    col1.metric(
+                        a,
+                        data.sender_msg_count(df1)[0]["message counts"][a],
+                        data.sender_msg_count(df1)[0]["message counts"][a]
+                        - data.sender_msg_count(df1)[1],
+                    )
+                    col2.metric(
+                        b,
+                        data.sender_msg_count(df1)[0]["message counts"][b],
+                        data.sender_msg_count(df1)[0]["message counts"][b]
+                        - data.sender_msg_count(df1)[1],
+                    )
+                except StopIteration:
+                    break
+            else:
+                try:
+                    col1, col2 = st.columns(2)
+                    a, b = next(iterator_marks), next(iterator_marks)
+                    col1.metric(
+                        a,
+                        data.sender_msg_count(df1)[0]["message counts"][a],
+                        data.sender_msg_count(df1)[0]["message counts"][a]
+                        - data.sender_msg_count(df1)[1],
+                    )
+                    col2.metric(
+                        b,
+                        data.sender_msg_count(df1)[0]["message counts"][a],
+                        data.sender_msg_count(df1)[0]["message counts"][b]
+                        - data.sender_msg_count(df1)[1],
+                    )
+                except StopIteration:
+                    break
+                else:
+                    c = next(iterator_marks)
+                    st.metric(
+                        c,
+                        data.sender_msg_count(df1)[0]["message counts"][c],
+                        data.sender_msg_count(df1)[0]["message counts"][c]
+                        - data.sender_msg_count(df1)[1],
+                    )
+
+        st.subheader("user vs message")
+
+        st.bar_chart(sender_msg)
+
+        st.subheader(f"Total media file shared -- {data.total_media_shared(df1)[0]}")
+
+        st.table(data.total_media_shared(df1)[1])
+
+        st.pyplot(data.word_cloud(df1)[0])
+
+        st.table(data.emojie_count(df1))
+
+        st.markdown("""---""")
+
+        col1, col2, col3, col4 = st.columns(4)
+
+        with col1:
+            st.write(f"Total words in the chat --  {data.word_letter_counter(df1)[3]}")
+        with col2:
+            st.write(
+                f"Total letters in the chat --  {data.word_letter_counter(df1)[2]}"
+            )
+
+        with col3:
+            st.write(f"Average words per message -- {data.word_letter_counter(df1)[1]}")
+        with col4:
+            st.write(
+                f"average letters per message   -- {data.word_letter_counter(df1)[0]}"
+            )
+
+        st.markdown("""---""")
+
+        st.pyplot(data.time_series(df1)[0])
+        st.markdown("""---""")
+        st.subheader(data.time_series(df1)[1])
+        st.subheader(data.time_series(df1)[2])
+        st.subheader(data.time_series(df1)[3])
+
+        st.markdown("""---""")
+        st.subheader(f"total deleted messages {data.word_letter_counter(df1)[4]}")
+        st.markdown("""---""")
+
+        st.pyplot(data.word_cloud(df1)[1])
+    except Exception as e:
+        st.subheader("please upload the doc in proper format")
---
+++ data.py
@@ -0,0 +1,322 @@
+import pandas as pd
+import re
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.gridspec import GridSpec
+from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
+from collections import Counter
+import demoji
+
+colorlist = [
+    "#D8DDEC",
+    "#D4EBD4",
+    "#EFEED8",
+    "#F9DFE1",
+    "#F6D0E8",
+    "#DFF2FD",
+    "#E2FCE6",
+    "#FCFADE",
+    "#FFEEE2",
+    "#FFDBDB",
+    "#F692BC",
+    "#F4ADC6",
+    "#FDFD95",
+    "#AAC5E2",
+    "#6891C3",
+    "#77DD77",
+]
+
+
+colorlist = [
+    "#D8DDEC",
+    "#D4EBD4",
+    "#EFEED8",
+    "#F9DFE1",
+    "#F6D0E8",
+    "#DFF2FD",
+    "#E2FCE6",
+    "#FCFADE",
+    "#FFEEE2",
+    "#FFDBDB",
+    "#F692BC",
+    "#F4ADC6",
+    "#FDFD95",
+    "#AAC5E2",
+    "#6891C3",
+    "#77DD77",
+]
+
+
+def clean_data(df):
+    def getdate(x):
+        res = re.search("\d\d/\d\d/\d\d", x)
+        if res != None:
+            return res.group()
+        else:
+            return ""
+
+    df["Date"] = list(map(lambda x: getdate(x), df.iloc[:, 0]))
+
+    ## Merge multiline chat data
+
+    for i in range(0, len(df)):
+        if df["Date"][i] == "":
+            c = i - 1
+            for j in range(i, len(df)):
+                if df["Date"][j] == "":
+                    df.iloc[c, 0] = " ".join([df.iloc[c, 0], df.iloc[j, 0]])
+
+                else:
+                    i = j - 1
+                    break
+        else:
+            df.iloc[i, 0] = df.iloc[i, 0]
+
+    ## Remove rows where date is empty
+    df.drop(np.where(df.iloc[:, 1] == "")[0], inplace=True)
+
+    def gettime(x):
+        res = re.search(".\d:\d\d\s[a|p]m", x)
+        if res != None:
+            return res.group()
+        else:
+            return ""
+
+    df["time"] = df[0].apply(gettime)
+    df["Hour"] = df["time"].apply(lambda x: x.split(":")[0])
+    df["Minute"] = df["time"].apply(lambda x: x.split(":")[1].split(" ")[0])
+    df["AmPm"] = df["time"].apply(lambda x: x.split(":")[1].split(" ")[1])
+
+    ## Extract Day Month and Year from Date
+    df["Day"] = list(map(lambda d: d.split("/")[0], df.Date))
+    df["Months"] = list(map(lambda d: d.split("/")[1], df.Date))
+    df["Year"] = list(map(lambda d: d.split("/")[2], df.Date))
+
+    ##Remove date from original text data using substitute function of regular expression
+    df.iloc[:, 0] = list(map(lambda x: re.sub("../../..", "", x)[2:], df.iloc[:, 0]))
+    ## Remove Timestamps from chat
+    df.iloc[:, 0] = list(
+        map(lambda x: re.sub(".*\d:\d\d\s[a|p]m", "", x)[2:], df.iloc[:, 0])
+    )
+
+    def getsender(x):
+        res = re.search(re.compile(".*?: "), x)
+        if res != None:
+            return res.group()[1:-2]
+        else:
+            return ""
+
+    df["sender"] = list(map(getsender, df.iloc[:, 0]))
+    df = df[df["sender"].notna()]
+
+    ## extract final message from chat data
+    def getmessage(x):
+        res = re.search(": .*", x)
+        if res != None:
+            return res.group()[2:]
+        else:
+            return None
+
+    df["Message"] = list(map(getmessage, df.iloc[:, 0]))
+
+    df = df.dropna()
+    df.drop([0], axis=1, inplace=True)
+    ## Reindex the dataframe
+    df = df.reset_index(drop=True)
+    return df
+
+
+def sender_msg_count(df):
+    op = (
+        df["sender"]
+        .value_counts()
+        .rename_axis("sender")
+        .to_frame("message counts")
+        .sort_values("message counts")
+    )
+    return op, round(op["message counts"].mean(), 0)
+
+
+def activity_wrt_time(df):
+
+    ## creating groups of data by time meridian
+    timemeridian = df.groupby(by="AmPm")
+
+    amhours = timemeridian.get_group("am")
+    pmhours = timemeridian.get_group("pm")
+
+    ## getting hourly activity counts
+    amhourcounts = amhours.Hour.value_counts().sort_index()
+    pmhourcounts = pmhours.Hour.value_counts().sort_index()
+
+    fig = plt.figure(figsize=[20, 10])
+
+    gs = GridSpec(2, 3)  # 2 rows and 3 columns
+    ax1 = fig.add_subplot(gs[0, 1])  # first row, second col
+    ax2 = fig.add_subplot(gs[1, 1])  # second row, second col
+    ax3 = fig.add_subplot(gs[:, 2])  # all row, third col
+
+    # Bar plot for messages shared in AM time meridian
+    ax1.bar(amhourcounts.index, amhourcounts.values, color="black")
+    ax1.set(xlabel="hour AM")
+    # Bar plot for messages shared in PM time meridian
+    ax2.bar(pmhourcounts.index, pmhourcounts.values, color="black")
+    ax2.set(xlabel="hour PM")
+
+    # Bar plot showing AM vs PM
+    ax3.bar(["AM", "PM"], [len(amhours), len(pmhours)], color="green")
+    ax3.annotate(
+        text=str(round(100 * len(amhours) / (len(amhours) + len(pmhours)))) + "%",
+        xy=[0, len(amhours) / 2],
+        color="white",
+        size=14,
+        horizontalalignment="center",
+    )
+    ax3.annotate(
+        text=str(round(100 * len(pmhours) / (len(amhours) + len(pmhours)))) + "%",
+        xy=[1, len(pmhours) / 2],
+        color="white",
+        size=14,
+        horizontalalignment="center",
+    )
+    ax3.set(title="percentage of message AM vs PM")
+    return fig
+
+
+def top_active_days_avgmsg(df):
+    top_10_active_days = (
+        df["Date"].value_counts().rename_axis("unique_values").to_frame("counts")
+    )
+    top_10_active_days.sort_values("counts", inplace=True)
+    avg_msg_per_day = top_10_active_days.mean()
+    avg_msg_per_day_num = round(avg_msg_per_day[0], 0)
+    top_10_active_daysnew = top_10_active_days[-10:]
+
+    # fig, ax = plt.subplots()
+    # ax.bar(x=top_10_active_daysnew.index, height=top_10_active_daysnew.values)
+    return top_10_active_daysnew, avg_msg_per_day_num
+
+
+def total_media_shared(df):
+    df["Media"] = (
+        df["Message"]
+        .astype(str)
+        .apply(lambda x: 1 if ("media omitted") in x.lower() else 0)
+    )
+    media = (
+        df[df["Media"] == 1]["sender"]
+        .value_counts()
+        .rename_axis("Members")
+        .to_frame("counts")
+    )
+    return media["counts"].sum(), media
+
+
+def word_cloud(df):
+    words = " ".join(df["Message"].apply(str))
+
+    def punctuation_stop(text):
+        filtered = []
+        stop_words = set(STOPWORDS)
+        word_tokens = text.split()
+        for w in word_tokens:
+            if w not in stop_words and w.isalpha():
+                filtered.append(w.lower())
+        return filtered
+
+    words_filtered = punctuation_stop(words)
+
+    text = " ".join([ele for ele in words_filtered])
+
+    wc = WordCloud(
+        background_color="white",
+        random_state=1,
+        stopwords=STOPWORDS,
+        max_words=50,
+        width=1000,
+        height=1500,
+    )
+    wordcloud = wc.generate(text)
+    fig1 = plt.figure()
+    plt.imshow(wordcloud)
+    plt.axis("off")
+
+    cmnwrd = Counter(words.split()).most_common(20)
+
+    wrd = []
+    cnt = []
+    for i in cmnwrd:
+        wrd.append(i[0])
+        cnt.append(i[1])
+
+    gs = pd.DataFrame(list(zip(cnt, wrd)), columns=["count", "words"]).set_index(
+        "words"
+    )
+
+    fig2 = plt.figure()
+    plt.plot(gs["count"], linestyle="-")
+    plt.xticks(rotation=60)
+    plt.ylabel("total message count")
+    return fig1, fig2
+
+
+def emojie_count(df):
+    li = []
+    for i in df["Message"].apply(str):
+
+        for j in demoji.findall(i).keys():
+            li.append(j)
+
+    d = Counter(li)
+    op = pd.DataFrame.from_dict(d, columns=["Count"], orient="index")
+    op = op.sort_values("Count", ascending=False)
+    return op[:10]
+
+
+def word_letter_counter(df):
+    df2 = df[df["Message"] == "<Media omitted>"]
+    df1 = df.copy()
+    df1 = df1.drop(df2.index)
+
+    df1["Letter_Count"] = df1["Message"].apply(str).apply(lambda s: len(s))
+    df1["Word_Count"] = df1["Message"].apply(str).apply(lambda s: len(s.split(" ")))
+
+    average_letter_per_message = round(df1["Letter_Count"].mean(), 0)
+    average_word_per_message = round(df1["Word_Count"].mean(), 0)
+    total_letters = df1["Letter_Count"].sum()
+    total_word = df1["Word_Count"].sum()
+    total_deleted_messages = df[df["Message"].str.contains("message was deleted")][
+        "Day"
+    ].count()
+
+    return (
+        average_letter_per_message,
+        average_word_per_message,
+        total_letters,
+        total_word,
+        total_deleted_messages,
+    )
+
+
+def time_series(df):
+    format = "%d/%m/%y %H:%M %p"
+    df["Datetime"] = pd.to_datetime(df["Date"] + " " + df["time"], format=format)
+    df_date = df.set_index(pd.DatetimeIndex(df["Datetime"]))
+    df_o = df_date.groupby("Date").count()["Day"]
+    fig1 = plt.figure(figsize=[15, 7])
+    plt.plot(df_o, linestyle="-")
+    plt.xticks(rotation=60)
+    plt.ylabel("total message count")
+
+    ini_time = df_date.index[0]
+    fin_time = df_date.index[-1]
+    tot_time = fin_time - ini_time
+
+    chat_started_datetime = "Chat started on - {} {}".format(
+        ini_time, df["AmPm"].iat[0]
+    )
+    last_chat_datetime = "Last chat on - {} {}".format(ini_time, df["AmPm"].iat[-1])
+    sttoend = "Out of {} days {} days members pinged.".format(
+        tot_time.days, df["Date"].nunique()
+    )
+    return fig1, chat_started_datetime, last_chat_datetime, sttoend
